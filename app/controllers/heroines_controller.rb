6be31919b9d03dd58fccdadbb8a07e7d7f9fd503
github username <github email address>
2019-08-14 11:03:59 -0400

    finished

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,27 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+
+    @heroine.save
+    redirect_to @heroine
+  end
+
+  private
+    def heroine_params
+      params.require(:heroine).permit(:name, :supername)
+    end
+
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,4 @@
 class Heroine < ApplicationRecord
+    belongs_to :power
+    validates :power, uniqueness: true
 end
