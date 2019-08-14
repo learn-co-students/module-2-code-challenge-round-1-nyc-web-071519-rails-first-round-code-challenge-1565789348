4b15438ac6c50be72c89b9190f4e5bf876c22cd7
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-08-14 11:03:11 -0400

    Done

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,9 @@
 class Heroine < ApplicationRecord
+    belongs_to :power
+
+
+    def power_name 
+        self.power.name if self.power
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  resources :powers, only: [:index, :show]
+  resources :heroines, only: [:index, :show, :new, :create]  
+
+
+
+
+  # get '/heroines/new', to: "heroines#new"
+  # post '/heroines', to: "heroines#create"
 end
