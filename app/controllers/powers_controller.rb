b428b0eaf0afbca756d363f3cdf474168423f2c6
Andrew Cheon <cheonandrew113@gmail.com>
2019-08-14 11:00:07 -0400

    finished

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,24 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  def new
+    @power = Power.new
+  end
+
+  def create
+    @power = Power.create(power_params)
+    redirect_to powers_path
+  end
+
+  private
+
+  def power_params
+    params.require(:power).permit(:name, :description)
+  end
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,5 @@
 class Heroine < ApplicationRecord
+    belongs_to :power
+
+    validates :super_name, presence: true, uniqueness: {message: "Name Already Taken!"}
 end
