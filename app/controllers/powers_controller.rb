d22bf951ca266daf9025dfb67093772844c3405a
github username <github email address>
2019-08-14 10:44:46 -0400

    finished

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,18 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  def new
+    @power = Power.new
+  end
+
+  def create
+    @power = Power.new(params.require(:power).permit(:name, :description))
+    @power.save
+    redirect_to power_path(@power)
+  end
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,12 @@
 class Heroine < ApplicationRecord
+    belongs_to :power
+
+    validates :super_name, uniqueness: true
+
+    def superpower
+        power = Power.all.find do |power|
+            power.id == self.power_id
+        end
+        power.name
+    end
 end
